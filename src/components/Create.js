1a68904e735fc88fdf8af2bdc5903801bea14508
VitalGa <vitalga10@gmail.com>
2024-10-02 23:47:19 +0300

    add loadDataFromForm and sendData

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,5 +1,44 @@
+import React, { useState } from 'react';
+import env from '../env.json';
+
 const Create = () => {
-  return <div></div>;
+  const [url, setUrl] = useState('');
+  let sendData = (obj) => {
+    fetch(env.urlBackend, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        if (response.result) {
+          setUrl(env.url + '/' + response.url);
+        }
+      });
+  };
+  let loadDataFromForm = (event) => {
+    event.preventDefault();
+    let note = event.target.elements.note.value;
+    note = note.trim();
+    if (note === '') {
+      alert('Заметка не может быть пустой');
+      return false;
+    }
+    sendData({ note: note });
+  };
+
+  return (
+    <div>
+      <form onSubmit={loadDataFromForm}>
+        <label htmlFor=''>Введите заметку</label>
+        <textarea name='note' id='note' defaultValue='test'></textarea>
+        <button type='submit'>Создать</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
